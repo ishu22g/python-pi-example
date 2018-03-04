d95ccb526f2449f67a0cf925338ffedcdbddc4e6
ishu22g <ishu22g@gmail.com>
2018-03-04 00:18:44 -0800

    new branch

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 
 def main(msg):
-   // no longer need comments
+   // no longer need comments 12
    print(msg)
 
 def secondry():
    print("New function")
 
 main("Hello world")
+
+
 
 
 
